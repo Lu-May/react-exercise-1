7f56a56bdc016c65f1646fb155b36805f60aec86
Lu-May <1054998345@qq.com>
2020-10-08 22:21:44 +0800

    [luyuehuan] feat:aboutme

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './AboutMe.css';
+
+class AboutMe extends React.Component {
+  render() {
+    return (
+      <div className="about-me">
+        <h3>ABOUT ME</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Repellendus,
+          non, dolorem, cumque distinctio magni quam expedita velit laborum sunt
+          amet facere tempora ut fuga aliquam ad asperiores voluptatem dolorum!
+          Quasi.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default AboutMe;
